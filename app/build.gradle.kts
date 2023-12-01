d1fc0a48bcac7f505c245be24aa510340ad7fb01
UnknownErrror <herobrine135111@gmail.com>
2023-12-01 16:02:40 +0300

    Format gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,75 +1,75 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+  alias(libs.plugins.androidApplication)
+  alias(libs.plugins.kotlinAndroid)
 }
 
 android {
-    namespace = "dev.zwander.localepickerproxy"
-    compileSdk = 34
+  namespace = "dev.zwander.localepickerproxy"
+  compileSdk = 34
 
-    defaultConfig {
-        applicationId = "dev.zwander.localepickerproxy"
-        minSdk = 33
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+    applicationId = "dev.zwander.localepickerproxy"
+    minSdk = 33
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables {
+      useSupportLibrary = true
     }
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.3"
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+  }
 }
 
 // Support for build.yml
 tasks.register("printVersionName") {
-    doLast {
-        println(android.defaultConfig.versionName)
-    }
+  doLast {
+    println(android.defaultConfig.versionName)
+  }
 }
 
 dependencies {
-    implementation(libs.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
-    implementation(platform(libs.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.material3)
-    implementation(libs.accompanist.drawablepainter)
-    implementation(libs.hiddenapibypass)
-    implementation(libs.material)
-    implementation(libs.coil.compose)
-    implementation(libs.squircle.shape)
+  implementation(libs.core.ktx)
+  implementation(libs.lifecycle.runtime.ktx)
+  implementation(libs.activity.compose)
+  implementation(platform(libs.compose.bom))
+  implementation(libs.ui)
+  implementation(libs.ui.graphics)
+  implementation(libs.ui.tooling.preview)
+  implementation(libs.material3)
+  implementation(libs.accompanist.drawablepainter)
+  implementation(libs.hiddenapibypass)
+  implementation(libs.material)
+  implementation(libs.coil.compose)
+  implementation(libs.squircle.shape)
 
-    debugImplementation(libs.ui.tooling)
+  debugImplementation(libs.ui.tooling)
 }
