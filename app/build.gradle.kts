e35e974a620bb2e873415e86b33a3f782a484b8d
UnknownErrror <herobrine135111@gmail.com>
2023-12-01 16:18:37 +0300

    Update gradle files Update .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,20 @@ plugins {
 
 android {
   namespace = "dev.zwander.localepickerproxy"
+
   compileSdk = 34
 
   defaultConfig {
     applicationId = "dev.zwander.localepickerproxy"
+
     minSdk = 33
     targetSdk = 34
+
     versionCode = 1
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     vectorDrawables {
       useSupportLibrary = true
     }
@@ -23,25 +27,26 @@ android {
   buildTypes {
     release {
       isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-      )
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
+
   kotlinOptions {
     jvmTarget = "17"
   }
+
   buildFeatures {
     compose = true
   }
+
   composeOptions {
     kotlinCompilerExtensionVersion = "1.5.3"
   }
+
   packaging {
     resources {
       excludes += "/META-INF/{AL2.0,LGPL2.1}"
